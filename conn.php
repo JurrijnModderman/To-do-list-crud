3a4e8ee4228fe86971389acb5f36c85f974f4d98
Unknown <99053654@mydavinci.nl>
2021-04-20 12:08:23 +0200

    new connection

--- conn.php
+++ conn.php
@@ -1,16 +1,15 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "mysql";
+$myDB = "To-do-list";
 
-// Create connection
-function connection() {
-	$conn = new mysqli($servername, $username, $password);
-
-	// Check connection
-	if ($conn->connect_error) {
-	  die("Connection failed: " . $conn->connect_error);
-	}
-	echo "Connected successfully";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$myDB", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
+
 ?>
