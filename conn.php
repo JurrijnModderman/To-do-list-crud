2bd25482c5337a22260aeb4cf8cf0ede6ed4d167
Unknown <99053654@mydavinci.nl>
2021-04-21 09:34:59 +0200

    trying to make a function of my connection part

--- conn.php
+++ conn.php
@@ -4,12 +4,14 @@ $username = "root";
 $password = "";
 $myDB = "to-do-list";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$myDB", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+function connection() {
+	try {
+    	$conn = new PDO("mysql:host=$servername;dbname=$myDB", $username, $password);
+    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    	echo "Connected successfully";
+	} catch(PDOException $e) {
+    	echo "Connection failed: " . $e->getMessage();
+	}
 }
 //pdo
 // $sql = "SELECT Id, Gebruiker, Taak FROM list";
--- index.php
+++ index.php
@@ -5,6 +5,12 @@
 </head>
 <body>
 	<?php include 'conn.php';?>
+	<?php
+		$sql = 'SELECT * FROM list';
+		$query = $conn->prepare($sql);
+		$query->execute();
+		$all = $query->fetchAll();
+	?>
 	<li><a href="create.php">Create</a></li>
 	<li><a href="update.php">Update</a></li>
 	<li><a href="delete.php">Delete</a></li>
