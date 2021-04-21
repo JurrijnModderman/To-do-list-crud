e617b6131709f94a23377aab381b68e4be59d4eb
Unknown <99053654@mydavinci.nl>
2021-04-21 11:45:42 +0200

    added a table to show the data inside the table in the db

--- index.php
+++ index.php
@@ -5,6 +5,24 @@
 </head>
 <body>
 	<?php include 'conn.php';?>
+
+<table style="border: 1px solid black">
+    <tr>
+        <th>Id | </th>
+        <th>Gebruiker | </th>
+        <th>Taak | </th>
+    </tr>
+ <?php
+  foreach ($all as $row) {
+ ?>   
+    <tr>
+        <td><?php echo $row['Id'] ?></td>
+        <td><?php echo $row['Gebruiker'] ?></td>
+        <td><?php echo $row['Taak'] ?></td>
+    </tr>
+<?php
+    }
+?>        
 	
 	<li><a href="create.php">Create</a></li>
 	<li><a href="update.php">Update</a></li>
