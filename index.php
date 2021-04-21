63927ce50edb35e09606c74d5b59a4ff6b9da249
Unknown <99053654@mydavinci.nl>
2021-04-21 10:11:19 +0200

    fixed it but not the way i wanted to

--- index.php
+++ index.php
@@ -4,8 +4,20 @@
 	<title>To-do-List</title>
 </head>
 <body>
-	<?php include 'conn.php';?>
+	<!-- <?php include 'conn.php';?> -->
 	<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$myDB = "to-do-list";
+
+	try {
+    	$conn = new PDO("mysql:host=$servername;dbname=$myDB", $username, $password);
+    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    	echo "Connected successfully";
+	} catch(PDOException $e) {
+    	echo "Connection failed: " . $e->getMessage();
+	}
 		$sql = 'SELECT * FROM list';
 		$query = $conn->prepare($sql);
 		$query->execute();
