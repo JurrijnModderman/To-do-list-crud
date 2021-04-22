c9139bfeff21ad128cd18627716512b7edceeb95
Unknown <99053654@mydavinci.nl>
2021-04-22 11:58:42 +0200

    trying to update the page when clicking on the button/link

--- update.php
+++ update.php
@@ -1,4 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>To-do-List</title>
+</head>
+<body>
+	<input type="Id" name="Id">
+	<input type="Gebruiker" name="Gebruiker">
+	<input type="Taak" name="Taak">
 <?php
 	include 'conn.php';
-	// header('Location: index.php');
+	function update() {
+		$conn = connection();
+		$id = $_GET['Id'];
+		$sql = 'UPDATE  FROM list WHERE Id = ' . $id;
+	}
+	update();
+	header('Location: index.php');
 ?>
+</body>
+</html>
