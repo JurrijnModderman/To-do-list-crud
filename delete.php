910e8fa3dfced79ab37ddbae0ebc6d55adf8392f
Unknown <99053654@mydavinci.nl>
2021-04-22 10:13:50 +0200

    saved it

--- delete.php
+++ delete.php
@@ -2,7 +2,8 @@
 	include 'conn.php';
 	function delete() {
 		$conn = connection();
-		$sql = 'Delete Id, Gebruiker, Taak WHERE Id = ['Id'], Gebruiker = ['Gebruiker'], Taak = ['Taak'] FROM list';
+		$id = $_GET['id']; 
+		$sql = 'DELETE  FROM list WHERE id = ' . $id;
 		$query = $conn->prepare($sql);
 		$query->execute();
 		$all = $query->fetchAll();
