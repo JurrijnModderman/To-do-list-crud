7fb48a1878f2fe61aced373d17521e9b112b88c8
Unknown <99053654@mydavinci.nl>
2021-04-28 14:00:55 +0200

    trying to update the data form the url

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
         <th>Id | </th>
         <th>Gebruiker | </th>
         <th>Taak | </th>
+        <td><a href="create.php">Create</a></td>
     </tr>
  <?php
   foreach ($all as $row) {
@@ -22,8 +23,7 @@
         <td><?php echo $row['Id'] ?></td>
         <td><?php echo $row['Gebruiker'] ?></td>
         <td><?php echo $row['Taak'] ?></td>
-        <td><a href="create.php">Create</a></td>
-		<td><a href="update.php?Id=<?php echo $row['Id']?>">Update</a></td>
+		<td><a href="update.php?Id=<?php echo $row['Id']?>,Gebruiker=<?php echo $row['Gebruiker']?>,Taak=<?php echo $row['Taak']?>">Update</a></td>
 		<td><a href="delete.php?Id=<?php echo $row['Id']?>">Delete</a></td>
     </tr>
 <?php
--- update.php
+++ update.php
@@ -8,9 +8,12 @@
 		include_once 'controller.php';
 	?>
 	<form action="update()" method="POST">
-		<?php $id = $_GET['Id']?>
-		Gebruiker:<input type="Gebruiker" name="gebruikerValue" value="">
-		Taak:<input type="Taak" name="taakValue" value="">
+		<?php $id = $_GET['Id'];
+		$gebruikerValue = $_GET['gebruikerValue'];
+		$taakValue = $_GET['taakValue'];
+		?>
+		Gebruiker:<input type="Gebruiker" name="gebruikerValue" value="<?php echo  $gebruikerValue?>">
+		Taak:<input type="Taak" name="taakValue" value="<?php echo $taakValue?>">
 		<input type="submit" value="Update" name="submit">
 	</form>
 <!-- <?php
