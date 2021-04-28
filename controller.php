07c818635189c5277bafdb6f5215b60f7b313e5a
Unknown <99053654@mydavinci.nl>
2021-04-28 10:23:06 +0200

    trying to call the function

--- controller.php
+++ controller.php
@@ -14,7 +14,9 @@
 	function update() {
 		$conn = connection();
 		$id = $_GET['Id'];
-		$sql = 'UPDATE list SET Gebruiker=gebruikerValue, Taak=taakValue WHERE Id = ' . $id;
+		$gebruikerValue = $_GET['gebruikerValue'];
+		$taakValue = $_GET['taakValue'];
+		$sql = 'UPDATE list SET Gebruiker=$gebruikerValue, Taak=$taakValue WHERE Id = ' . $id;
 		$query = $conn->prepare($sql);
 		$query->execute();
 		$all = $query->fetchAll();
