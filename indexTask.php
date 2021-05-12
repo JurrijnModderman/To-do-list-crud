620ee3f324edcc9904bda183c69af60314098c3b
Unknown <99053654@mydavinci.nl>
2021-05-12 14:33:00 +0200

    added another view file for another list

--- indexTask.php
+++ indexTask.php
@@ -11,19 +11,23 @@
 
 <table style="border: 1px solid black">
     <tr>
+        <th>List_Id | </th>
         <th>Id | </th>
-        <th>Gebruiker | </th>
-        <th>Taak | </th>
+        <th>Description | </th>
+        <th>Time | </th>
+        <th>Status | </th>
         <td><a href="create.php">Create</a></td>
-        <td><a href="indexTask.php">Takenlijst</a></td>
+        <td><a href="index.php">Algemene Lijst</a></td>
     </tr>
  <?php
   foreach ($all as $row) {
  ?>   
     <tr>
+        <td><?php echo $row['List_Id'] ?></td>
         <td><?php echo $row['Id'] ?></td>
-        <td><?php echo $row['Gebruiker'] ?></td>
-        <td><?php echo $row['Taak'] ?></td>
+        <td><?php echo $row['Description'] ?></td>
+        <td><?php echo $row['Time'] ?></td>
+        <td><?php echo $row['Status'] ?></td>
 		<td><a href="update.php?Id=<?php echo $row['Id']?>&Gebruiker=<?php echo $row['Gebruiker']?>&Taak=<?php echo $row['Taak']?>">Update</a></td>
 		<td><a href="delete.php?Id=<?php echo $row['Id']?>">Delete</a></td>
     </tr>
