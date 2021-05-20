1a68cba5deadd9bc0f57a2b1d970d463d351e178
Unknown <99053654@mydavinci.nl>
2021-05-20 13:51:03 +0200

    changed the query and it works now

--- updateFUNC.php
+++ updateFUNC.php
@@ -5,10 +5,9 @@
 		$id = $_POST['Id'];
 		$User = $_POST['User'];
 		$Task_Name = $_POST['Task_Name'];
-		$sql = 'UPDATE list SET User=$User, Task_Name=$Task_Name WHERE Id = ' . $id;
+		$sql = "UPDATE list SET User='$User', Task_Name='$Task_Name' WHERE Id = " . $id;
 		$query = $conn->prepare($sql);
 		$query->execute();
-		$all = $query->fetchAll();
 		header('Location: index.php');
 	}
 	update();
