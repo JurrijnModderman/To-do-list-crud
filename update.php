f859ab83b58a00246574725af29e7cb2b2bb4824
Unknown <99053654@mydavinci.nl>
2021-05-20 16:31:46 +0200

    added the files to update the tasks

--- update.php
+++ update.php
@@ -5,7 +5,6 @@
 </head>
 <body>
 	<?php 
-		// include_once 'controller.php';
 		include 'conn.php';
 		$id = $_GET['Id'];
 		$User = $_GET['User'];
@@ -13,24 +12,9 @@
 	?>
 	<form action="updateFUNC.php" method="POST">
 		<input type="hidden" name="Id" value="<?php echo $id; ?>">
-		<!-- <input type="hidden" name="gebruiker" value="<?php echo $gebruiker; ?>">
-		<input type="hidden" name="taak" value="<?php echo $taak; ?>"> -->
 		User:<input type="User" name="User" value="<?php echo $User;?>">
 		Task_Name:<input type="Taak" name="Task_Name" value="<?php echo $Task_Name;?>">
 		<input type="submit" value="Update" name="submit">
 	</form>
-<!-- <?php
-	include 'controller.php';
-	// update();
-	// function update() {
-	// 	$conn = connection();
-	// 	$id = $_GET['Id'];
-	// 	$sql = 'UPDATE list SET Gebruiker=gebruikerValue, Taak=taakValue WHERE Id = ' . $id;
-	// 	$query = $conn->prepare($sql);
-	// 	$query->execute();
-	// 	$all = $query->fetchAll();
-	// }
-	// header('Location: index.php');
-?> -->
 </body>
 </html>
--- updateTaskFUNC.php
+++ updateTaskFUNC.php
@@ -6,7 +6,7 @@
 		$Description = $_POST['Description'];
 		$Time = $_POST['Time'];
 		$Status = $_POST['Status'];
-		$sql = "UPDATE list SET Description='$Description', Time='$Time', Status='$Status' WHERE Id = " . $id;
+		$sql = "UPDATE task SET Description='$Description', Time='$Time', Status='$Status' WHERE Id = " . $id;
 		$query = $conn->prepare($sql);
 		$query->execute();
 		header('Location: indexTask.php');
