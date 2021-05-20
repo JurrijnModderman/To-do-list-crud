2c67d319e68295ba3a960223ab365679b67784aa
Unknown <99053654@mydavinci.nl>
2021-05-20 11:51:29 +0200

    added the files for the create tasks button

---
+++ createTask.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>To-do-List</title>
+</head>
+<body>
+	<?php
+	include 'conn.php';
+	?>
+	
+	<form action="createFUNC.php" method="POST">
+		User:<input type="User" name="User" value="">
+		Task_Name:<input type="Task_Name" name="Task_Name" value="">
+		<input type="submit" value="Create" name="submit">
+	</form>
+</body>
+</html>
---
+++ createTaskFunc.php
@@ -0,0 +1,18 @@
+<?php
+	include 'conn.php';
+	function create() {
+		$conn = connection();
+		$User = $_POST['User'];
+		$Task_Name = $_POST['Task_Name'];
+		$sql = "INSERT INTO List (User, Task_Name)
+  		VALUES ('$User', '$Task_Name')";
+		$query = $conn->prepare($sql);
+		$query->execute();
+		$all = $query->fetchAll();
+		header('Location: index.php');
+	}
+	create();
+
+
+
+?>
