31cc5b8541bc79b732bcd613cece53efc1df62ff
Unknown <99053654@mydavinci.nl>
2021-05-26 11:15:51 +0200

    deleted the old 'style code'

--- indexTask.php
+++ indexTask.php
@@ -9,13 +9,6 @@
 	include 'conn.php';
 	$all = getDataTasks();
 	?>
-<!-- <table style="border: 1px solid black">
-    <tr>
-        <th>Description | </th>
-        <th>Time | </th>
-        <th>Status | </th>
-        <td><a href="index.php">Back</a></td>
-    </tr> -->
     <div class="container">
     <table class="table" style="border: 1px solid black;">
   <thead>
@@ -50,14 +43,7 @@
     }
 ?>
   </tbody>
-</table>
-    <!-- <tr>
-        <td><?php echo $row['Description'] ?> |</td>
-        <td><?php echo $row['Time'] ?> |</td>
-        <td><?php echo $row['Status'] ?> |</td>
-		<td><a href="updateTask.php?List_Id=<?php echo $row['List_Id']?>&Id=<?php echo $row['Id']?>&Description=<?php echo $row['Description']?>&Time=<?php echo $row['Time']?>&Status=<?php echo $row['Status']?>">Update</a></td>
-		<td><a href="deleteTask.php?Id=<?php echo $row['Id']?>&List_Id=<?php echo $row['List_Id']?>">Delete</a></td>
-    </tr> -->     
+</table>   
     <tr>
         <td><a href='createTask.php?List_Id=<?php echo $_GET['List_Id']?>&Task_Name=<?php echo $_GET['Task_Name']?>'>Create</a></td> 
     </tr>  
